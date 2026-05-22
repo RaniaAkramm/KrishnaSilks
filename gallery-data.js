703d735590063d31d26c2279c2b5eb23cfa889d6
RaniaAkramm <ssess535@gmail.com>
2026-05-22 21:26:39 +0300

    Delete gallery-data.js

--- gallery-data.js
+++
@@ -1,7 +0,0 @@
-const imagesData = [
-    "https://ibb.co/zV4MKCD3",
-    "https://ibb.co/Mk2gYQ7M",
-    "https://i.imgur.com/Qj4S32i.jpeg",
-    "https://i.imgur.com/zW6t2gY.jpeg",
-    "https://i.imgur.com/eH2T7gW.jpeg"
-];
