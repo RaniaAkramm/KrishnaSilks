1e258e5c0085807cc23cd36a8246d620ecf55054
RaniaAkramm <ssess535@gmail.com>
2026-05-22 21:20:42 +0300

    Update gallery-data.js

--- gallery-data.js
+++ gallery-data.js
@@ -1,6 +1,6 @@
 const imagesData = [
     "https://ibb.co/zV4MKCD3",
-    "https://i.imgur.com/YJpY9uI.jpeg",
+    "https://ibb.co/Mk2gYQ7M",
     "https://i.imgur.com/Qj4S32i.jpeg",
     "https://i.imgur.com/zW6t2gY.jpeg",
     "https://i.imgur.com/eH2T7gW.jpeg"
