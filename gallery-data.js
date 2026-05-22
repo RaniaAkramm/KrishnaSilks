9d67e53261faccb004c3e9bcf8b019f4d340704e
RaniaAkramm <ssess535@gmail.com>
2026-05-22 20:36:18 +0300

    Create gallery-data.js

---
+++ gallery-data.js
@@ -0,0 +1,7 @@
+const imagesData = [
+    "https://i.imgur.com/gKq9yG8.jpeg",
+    "https://i.imgur.com/YJpY9uI.jpeg",
+    "https://i.imgur.com/Qj4S32i.jpeg",
+    "https://i.imgur.com/zW6t2gY.jpeg",
+    "https://i.imgur.com/eH2T7gW.jpeg"
+];
